3448de7e5f191c3cfd408e48bcfadb1aed632305
AnastasiiaAnosova <118612211+AnastasiiaAnosova@users.noreply.github.com>
2023-12-26 22:35:03 +0100

    processing to hooks implem.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,71 +1,139 @@
-import { Component } from "react";
+import { useEffect, useRef, useState } from "react";
 import { nanoid } from "nanoid";
 import CreateContact from '../CreateContact';
 import ContactList from "../ContactList";
 import SearchContact from "../SearchContact";
-import { AppContainer, Header } from "./App.styled"; 
+import { AppContainer, Header } from "./App.styled";
 
-class App extends Component {
+const App = () => {
 
-  state = {
-    contacts: [],
-    filter: '',
-  };
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const localData = localStorage.getItem('contacts');
-    if (localData && JSON.parse(localData).length) {
-      this.setState({ contacts: JSON.parse(localData) });
+  // option to save contacts between renders
+  // const prevContactsRef = useRef();
+  // useEffect(() => {
+  //   prevContactsRef.current = contacts;
+  // }, [contacts]);
+
+
+  // useEffect(() => {
+  //   const prevLocalData = localStorage.getItem('contacts');
+  //   if (prevLocalData && JSON.parse(prevLocalData).length) {
+  //     setContacts(JSON.parse(prevLocalData));
+  //   }
+  // }, []);
+
+  // useEffect(() => {
+  //   const prevLocalData = localStorage.getItem('contacts');
+  //   if (contacts !== prevLocalData) {
+  //     localStorage.setItem('contacts', JSON.stringify(contacts));
+  //   }
+  // }, [contacts]);
+
+  useEffect(() => {
+    const prevLocalData = localStorage.getItem('contacts');
+    if (prevLocalData && JSON.parse(prevLocalData).length) {
+      setContacts(JSON.parse(prevLocalData));
     }
-  }
 
-  componentDidUpdate(_, prevState) {
-    const currentContacts = this.state.contacts;
-    if (currentContacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(currentContacts));
+    if (contacts !== prevLocalData) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
     }
-  }
+  }, [contacts]);
 
-  createContact = (contact) => {
-    const isAlreadyExist = this.state.contacts.some((el) => el.name.toLowerCase() === contact.name.toLowerCase());
-    if (isAlreadyExist) return alert(`${contact.name} is already in contacts.`);
-    const newContact = {
-      ...contact,
+  const createContact = (newContact) => {
+    const isAlreadyExist = contacts.some(el => el.name.toLowerCase() === newContact.name.toLowerCase());
+    if (isAlreadyExist) return alert(`${newContact.name} is already in contacts.`);
+
+    const newContactCreate = {
+      ...newContact,
       id: nanoid(),
     }
-    this.setState((prev) => ({
-      contacts: [newContact, ...prev.contacts],
-    }));
-  }  
-
-  handleSearchChange = (e) => {
-    this.setState({ filter: e.target.value });
-  };
-
-  searchContact = () => {
-    const { contacts, filter } = this.state;
-    return contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-  };
-
-  deleteContact = (id) => {
-    this.setState((prev) => ({
-      contacts: prev.contacts.filter(el => el.id !== id),
-    }))
+    setContacts((prev) => [newContactCreate, ...prev]);
   }
+  // createContact = (contact) => {
+  //   const isAlreadyExist = this.state.contacts.some(
+  //     (el) => el.name.toLowerCase() === contact.name.toLowerCase()
+  //   );
+  //   if (isAlreadyExist) return alert(`${contact.name} is already in contacts.`);
+  //   const newContact = {
+  //     ...contact,
+  //     id: nanoid(),
+  //   }
+  //   this.setState((prev) => ({
+  //     contacts: [newContact, ...prev.contacts],
+  //   }));
+  // }  
 
-  render() {
-    const filteredContacts = this.searchContact();
-    return (
-      <AppContainer>
-        <Header>Phonebook</Header>
-        <CreateContact createContact={this.createContact} />
-        <Header>Contacts</Header>
-        <SearchContact handleSearchChange={this.handleSearchChange} />
-        <ContactList contacts={filteredContacts} deleteContact={this.deleteContact} />
-      </AppContainer>
-    );
-  }
+  return (
+    <div>App</div>
+  )
 }
-export default App;
+
+export default App
+
+// class App extends Component {
+
+//   state = {
+//     contacts: [],
+//     filter: '',
+//   };
+
+//   componentDidMount() {
+//     const localData = localStorage.getItem('contacts');
+//     if (localData && JSON.parse(localData).length) {
+//       this.setState({ contacts: JSON.parse(localData) });
+//     }
+//   }
+
+//   componentDidUpdate(_, prevState) {
+//     const currentContacts = this.state.contacts;
+//     if (currentContacts !== prevState.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(currentContacts));
+//     }
+//   }
+
+//   createContact = (contact) => {
+//     const isAlreadyExist = this.state.contacts.some((el) => el.name.toLowerCase() === contact.name.toLowerCase());
+//     if (isAlreadyExist) return alert(`${contact.name} is already in contacts.`);
+//     const newContact = {
+//       ...contact,
+//       id: nanoid(),
+//     }
+//     this.setState((prev) => ({
+//       contacts: [newContact, ...prev.contacts],
+//     }));
+//   }  
+
+//   handleSearchChange = (e) => {
+//     this.setState({ filter: e.target.value });
+//   };
+
+//   searchContact = () => {
+//     const { contacts, filter } = this.state;
+//     return contacts.filter((contact) =>
+//       contact.name.toLowerCase().includes(filter.toLowerCase())
+//     );
+//   };
+
+//   deleteContact = (id) => {
+//     this.setState((prev) => ({
+//       contacts: prev.contacts.filter(el => el.id !== id),
+//     }))
+//   }
+
+//   render() {
+//     const filteredContacts = this.searchContact();
+//     return (
+//       <AppContainer>
+//         <Header>Phonebook</Header>
+//         <CreateContact createContact={this.createContact} />
+//         <Header>Contacts</Header>
+//         <SearchContact handleSearchChange={this.handleSearchChange} />
+//         <ContactList contacts={filteredContacts} deleteContact={this.deleteContact} />
+//       </AppContainer>
+//     );
+//   }
+// }
+// export default App;
